9860f09df356f08d6460a7ed8cd0baaf6532549c
Suhail Mirza <mirza.suhail@gmail.com>
2025-09-14 16:04:01 +0530

    api.js changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,21 @@
-import axios from "axios";
+import axios from "axios"
 
-const api=axios.create({
-    baseURL:"http://localhost:5000/api/"
+const local = "http://localhost:5000/"
+const production="https://backend-ecommerce-api-2zqg.onrender.com"
+
+let api_url=""
+let mode="pro"
+
+if(mode==="pro"){
+    api_url=production
+}
+else{
+    api_url=local
+}
+
+const api = axios.create({
+    baseURL : `${api_url}api`,
+    withCredentials:true
 })
 
-export default api;
+export default api
